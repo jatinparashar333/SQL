3f954ac978bf72a75a3bdb5050c37c716efc55bf
Jatin Parashar <78733719+jatinparashar333@users.noreply.github.com>
2022-09-28 07:44:57 +0530

    Time: 1276 ms (9.28%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
 
+# 1) concat function :
+#               takes a variable number of string arguments and concatenates (or joins) them                   into a single string.
+#                takes minimum 2 input 
+#               convert each input in string implicitly
+# 2) upper function : 
+#                convert string into uppercase . SYNTAX -> UPPER (STRING)
+# 3) lower function : 
+#                convert string into lowercase . SYNTAX -> LOWER (STRING)
+# 4) left function : 
+#                extract number of characters from a string. SYNTAX -> LEFT (STRING , NO_OF_CHARS) - starting from left 
+# 5) right function :
+#                extrat number of characters from a string. SYNTAX -> RIGHT (STRING , NO_OF_CHARS) - starting from right 
+# 6) length function : 
+#                 return length of string. SYNTAX -> LENGTH(string)
 
 
 
