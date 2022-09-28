8380a299a08f64caededddcb0c3c687deaef3337
Jatin Parashar <78733719+jatinparashar333@users.noreply.github.com>
2022-09-28 08:14:21 +0530

    Time: 487 ms (67.05%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+
+
+# Almost immediately detected, the only serious challange of this problem is how to aggregate # the product names in one cell. In MySql, this can be done using GROUP_CONCAT, in which you 
+# can also specify the sorting mechanism for the group concatenation (aggregation). The rest #is simple.
+
+
+
 select sell_date,count(distinct product) as num_sold,
 group_concat(distinct product order by product separator ',') as products
 from activities group by sell_date;
